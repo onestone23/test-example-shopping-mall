d361f7d1e8d260bd295436a01829cad8b3fdb8fb
onestone23 <lws2269@kakao.com>
2024-08-23 17:18:31 +0900

    Error,Empty

--- src/pages/cart/components/tests/EmptyNotice.spec.jsx
+++ src/pages/cart/components/tests/EmptyNotice.spec.jsx
@@ -4,4 +4,22 @@ import React from 'react';
 import EmptyNotice from '@/pages/cart/components/EmptyNotice';
 import render from '@/utils/test/render';
 
-it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {});
+// 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행
+// useNavigate 훅으로 반환받은 navigate 함수가 올바르게 호출되었는가 -> 스파이 함수
+const navigateFn = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  // 컴포넌트 내부에서 useNavigate호출시 선언한 spy 함수로 대체
+  return { ...original, useNavigate: () => navigateFn };
+});
+
+it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {
+  const { user } = await render(<EmptyNotice />);
+
+  await user.click(screen.getByText('홈으로 가기'));
+
+  // 1, '/' 인자가 들어갔는지 검증
+  expect(navigateFn).toHaveBeenCalledWith(1, '/');
+});
--- src/pages/error/components/tests/ErrorPage.spec.jsx
+++ src/pages/error/components/tests/ErrorPage.spec.jsx
@@ -4,4 +4,19 @@ import React from 'react';
 import ErrorPage from '@/pages/error/components/ErrorPage';
 import render from '@/utils/test/render';
 
-it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {});
+const navigateFn = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  // 컴포넌트 내부에서 useNavigate호출시 선언한 spy 함수로 대체
+  return { ...original, useNavigate: () => navigateFn };
+});
+
+it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
+  const { user } = await render(<ErrorPage />);
+
+  await user.click(screen.getByRole('button', { name: '뒤로 이동' }));
+
+  expect(navigateFn).toHaveBeenCalledWith(-1);
+});
