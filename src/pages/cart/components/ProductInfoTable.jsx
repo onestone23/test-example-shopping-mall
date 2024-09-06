616b805daa385cafeb14d90c36bd57e32ee1e2f1
onestone23 <lws2269@kakao.com>
2024-09-06 16:43:22 +0900

    주석작성

--- src/pages/cart/components/ProductInfoTable.jsx
+++ src/pages/cart/components/ProductInfoTable.jsx
@@ -6,6 +6,12 @@ import { useCartStore } from '@/store/cart';
 import { useUserStore } from '@/store/user';
 import { pick } from '@/utils/common';
 
+// cart state 변경에 따른 UI 변경도 검증 -> 실제 앱의 기능과 유사
+// ProductInfoTableRow 컴포넌트의 기능까지 모두 검증할 수 있음
+
+// ProductInfoTableRow에서 state나 액션을 가져오는 경우
+// -> 상태 관리 코드 산재 -> 로직 파악 및 테스트 파악이 어려움
+// state, api에 대한 제어 코드를 통합 테스트 대상 컴포넌트로 응집 -> 유지보수성 향상, 테스트의 단위 나누기 좋음
 const ProductInfoTable = () => {
   // 테스트 실행 전에 zustand 스토어의 state를 원하는 대로 변경 필요
   const { cart, removeCartItem, changeCartItemCount } = useCartStore(state =>
