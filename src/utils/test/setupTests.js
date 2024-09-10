756045aa24b3d9c7c19f6d713347662a4123a0c9
onestone23 <lws2269@kakao.com>
2024-09-10 14:47:07 +0900

    주석작성

--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -5,7 +5,8 @@ import { handlers } from '@/__mocks__/handlers';
 
 /* msw */
 export const server = setupServer(...handlers);
-
+// msw 설정 적용
+// -> 테스트 환경에서 API 호출은 msw의 핸들러에 설정한 응답으로 모킹
 beforeAll(() => {
   server.listen();
 });
